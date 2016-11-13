db7606bf8102625252f45502bc3f240f02eb2a6f
Abhi <abhish.ab@gmail.com>
2016-11-12 19:56:39 -0800

    modifying cpu info

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Edge Computing Embedded Platform
-Developed by Abhishek Gurudutt, Chinmayi Divakara
-Praveen Prabhakaran, Tejeshwar Chandra Kamaal
-"""
-
-import ecep_docker as ecep_docker
-import ecep_wampClient as ecep_wampClient
--- ecep_docker/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Edge Computing Embedded Platform
-Developed by Abhishek Gurudutt, Chinmayi Divakara
-Praveen Prabhakaran, Tejeshwar Chandra Kamaal
-"""
-
-from container import *
-from cpu_info import *
