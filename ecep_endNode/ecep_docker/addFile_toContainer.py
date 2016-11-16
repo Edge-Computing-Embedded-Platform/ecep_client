dce5de794f0c9a1bd4024723d76849b5c0ffba8b
tejeshwar1991 <tejaskchandra@gmail.com>
2016-11-15 23:20:00 -0800

    Adding init routine to class definition

--- ecep_endNode/ecep_docker/addFile_toContainer.py
+++ ecep_endNode/ecep_docker/addFile_toContainer.py
@@ -16,72 +16,86 @@ import time
 invoke_cli = Client(base_url='unix://var/run/docker.sock')
 
 class addFile:
+	"""
+	This class definitions handle transferring a file into containers and executing them inside a container. 
+	"""
+	def __init__(self):
+		self._containerName = None
+		self._localPath = None
+		self._containerPath = None
+		self._user = 'root'
+		self._fileType = None
+		self._status = {}
+		self._command = []
+		
 	def startContainer_toAddFile(self, **kwargs):
+		"""
+		Starts a container to setup the file transfer.
+		"""
 		print ('startConatiner')
-		containerID = kwargs['container_name']
-	        status = {}
+		self._containerName = kwargs['container_name']
 		
 		try:
-			response = invoke_cli.start(containerID)
-			status["status"] = "True"
+			response = invoke_cli.start(self._containerName)
+			self._status["status"] = "True"
 		except:
-			status["status"] = "False"
+			self._status["status"] = "False"
 		
-		return json.dumps(status)
+		return json.dumps(self._status)
 			
 
 	def copyFileTo_container(self, **kwargs):
+		"""
+		Copies a file to container. 
+		"""
 		print ('copyFile routine')
-		global filetype, containerID, user
-		containerID = kwargs['container_name']	
-		path = kwargs['containerpath']
-		data = kwargs['file']
-		localpath = kwargs['local_path']
+		self._containerName = kwargs['container_name']	
+		self._containerPath = kwargs['containerpath']
+		self._localPath = kwargs['local_path']
 		
 		try:
-			get_fileObj = open(localpath)
+			_fileObj = open(self._localPath)
 		except OSError as err:
 			print err.errno == errno.EPERM
 			
-		filetype =  os.path.basename(localpath)
-		print ('filetype: ',filetype)
-		pw_tarstream = BytesIO()
-		pw_tar = tarfile.TarFile(fileobj=pw_tarstream, mode='w') #class for reading and writing tar archives. 
-		file_data = get_fileObj
-		tarinfo = tarfile.TarInfo(name=filetype) #create an object
-		tarinfo.size = os.path.getsize(localpath)
-		tarinfo.mtime = time.time()
-		pw_tar.addfile(tarinfo, get_fileObj)
-		pw_tar.close()
-		pw_tarstream.seek(0)
+		self._fileType =  os.path.basename(self._localPath)
+		print ('filetype: %s'%'self._fileType')
+		_pw_tarstream = BytesIO()
+		_pw_tar = tarfile.TarFile(fileobj=_pw_tarstream, mode='w') #class for reading and writing tar archives. 
+		_tarinfo = tarfile.TarInfo(name=self._fileType) #create an object
+		_tarinfo.size = os.path.getsize(self._localPath)
+		_tarinfo.mtime = time.time()
+		_pw_tar.addfile(_tarinfo, _fileObj)
+		_pw_tar.close()
+		_pw_tarstream.seek(0)
 		
-        	_putArchive_response = invoke_cli.put_archive(containerID,path,pw_tarstream)
+        	_putArchive_response = invoke_cli.put_archive(self._containerName,self._containerPath,_pw_tarstream)
         	print ('_putArchive_response: ',_putArchive_response)
         	
         	if _putArchive_response: 
-        		_script = {'container_name': containerID, 'user':'root'}
-        		print ('container_name: ',containerID) 
+        		_script = {'container_name': self._containerName, 'user':self._user}
+        		print ('container_name: ',self._containerName) 
         		self.run_shellScript(**_script)
         		
         	return _putArchive_response
         	
-        def run_shellScript(self, **kwargs):	
+        def run_shellScript(self, **kwargs):
+        	"""
+        	Executes the transferred file.
+        	"""	
         	print('run_shell routine')	
-		containerID = kwargs['container_name']	
-		#print ('con_name: ',containerID)
-		#stdout = kwargs['stdout']
-		user = kwargs['user']
-		exe_file = filetype
-		print ('executableFile: ',exe_file)
-		_file_location = os.path.join("/",exe_file)
-		print _file_location
-		cmmd = ['sh',_file_location]
-		print ('containerID_shellroutine: ,user: ',containerID,user)
-		response = invoke_cli.exec_create(container=containerID,cmd=cmmd,user='root')
-		print ('response: ',response)
-		ID = response
-		response1 = invoke_cli.exec_start(exec_id = ID)
-		print ('response1: ',response1) 
+		self._containerName = kwargs['container_name']	
+		self._user = kwargs['user']
+		_execFile = self._fileType
+		print ('executableFile: ',_execFile)
+		_fileLocation = os.path.join("/",_execFile)
+		print _fileLocation
+		self._command = ['sh',_fileLocation]
+		print ('containerID_shellroutine: ,user: ',self._containerName,self._user)
+		_execCreate_response = invoke_cli.exec_create(container=self._containerName,cmd=self._command,user=self._user)
+		print ('_execCreate_response: ',_execCreate_response)
+		_execStart_response1 = invoke_cli.exec_start(exec_id = _execCreate_response)
+		print ('_execStart_response1: ',_execStart_response1) 
 		
 
 if __name__ == "__main__":
