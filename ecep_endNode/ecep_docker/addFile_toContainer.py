fbe2f9fdfc74bc249563bc4dda71818744fc5ce3
tejeshwar1991 <tejaskchandra@gmail.com>
2016-11-16 23:07:41 -0800

    Code restructing

--- ecep_endNode/ecep_docker/addFile_toContainer.py
+++ ecep_endNode/ecep_docker/addFile_toContainer.py
@@ -97,7 +97,9 @@ class addFile:
 		(self._folderName, self._extension) = os.path.splitext(self._fileName)
 		print ('folderName: extension: ',self._folderName, self._extension)
 		
-		_transferApp = {'container_name': self._containerName,'containerpath': self._containerPath, 'local_path': self._localPath}
+		_transferApp = {'container_name': self._containerName,\
+				'containerpath': self._containerPath, \
+				'local_path': self._localPath}
 		
 		_successful = self.transferFile(**_transferApp)
 		
@@ -106,13 +108,41 @@ class addFile:
 			print self._localPath
 			
 			_transferScript = {'container_name': self._containerName, \
-			'containerpath': self._containerPath,\
-			'local_path': self._localPath}
+					   'containerpath': self._containerPath,  \
+					   'local_path': self._localPath}
 			
 			_checkStatus = self.transferFile(**_transferScript)
 			
 			if _checkStatus:
-				_executeScript = {'container_name':self._containerName,'user':self._user}
+				_execFile = self._fileType
+				print ('executableFile: ',_execFile)
+				_fileLocation_inContainer = os.path.join("/home/",_execFile)
+				print ('_fileLocation_inContainer: ',_fileLocation_inContainer)
+				
+				_executeScript = {'container_name':self._containerName,\
+						  'user':self._user,\
+						  '_execFile':_execFile,\
+						  '_filePath_inContainer':_fileLocation_inContainer
+						  }	
+				print ('containerName: ,containerPath: ,localPath: execFile: ,filePath_inContainer: ',
+				self._containerName,\
+				self._containerPath,\
+				self._localPath,\
+				_execFile,\
+				_fileLocation_inContainer)					  
+
+				self.run_shellScript(**_executeScript)
+				 
+				_execFile = self._requirement_sh
+				print ('executable_shellFile: ',_execFile)
+				_fileLocation_inContainer = os.path.join("/home/",self._folderName,_execFile)
+				print ('_fileLocation_inContainer: ',_fileLocation_inContainer)
+				
+				_executeScript = {'container_name':self._containerName,\
+						  'user':self._user,\
+						  '_execFile':_execFile,\
+						  '_filePath_inContainer':_fileLocation_inContainer
+						  }
 				self.run_shellScript(**_executeScript)
 				
 		return _checkStatus
@@ -124,49 +154,31 @@ class addFile:
         	print('run_shell routine')	
 		self._containerName = kwargs['container_name']	
 		self._user = kwargs['user']	
-
-		_execFile = self._fileType
-		print ('executableFile: ',_execFile)
-		_fileLocation_inContainer = os.path.join("/home/",_execFile)
-		print ('_fileLocation_inContainer: ',_fileLocation_inContainer)
+		_execFile = kwargs['_execFile']
+		_fileLocation_inContainer = kwargs['_filePath_inContainer']
+		
 		self._command = ['sh',_fileLocation_inContainer]
 		print ('containerID_shellroutine: ,user: ',self._containerName,self._user)
 		
-		_execCreate_response = invoke_cli.exec_create( container=self._containerName,
-							       cmd=self._command,\
-							       user=self._user,\
-							       stderr=False,\
-							       tty = False,\
-							       stdout=False)
-							       
+		_execCreate_response = invoke_cli.exec_create( container = self._containerName,
+							       cmd = self._command,\
+							       user = self._user)				       
 		print ('_execCreate_response: ',_execCreate_response)
-		_execStart_response = invoke_cli.exec_start(exec_id = _execCreate_response,\
-							     detach = True,\
-							     tty = False,\
-							     stream=True)
-	
-		print ('_execStart_response1: ',_execStart_response) 
-		_execFile = self._requirement_sh
-		print ('executable_shellFile: ',_execFile)
-		_fileLocation_inContainer = os.path.join("/home/",self._folderName,_execFile)
-		print ('_fileLocation_inContainer: ',_fileLocation_inContainer)
-		self._command = ['sh',_fileLocation_inContainer]
-		print ('containerID_shellroutine: ,user: ',self._containerName,self._user)
-		_execCreate_response = invoke_cli.exec_create(container=self._containerName,cmd=self._command,user=self._user)
-		print ('_execCreate_response: ',_execCreate_response)
-		_execStart_response = invoke_cli.exec_start(exec_id = _execCreate_response)
-		print ('_execStart_response1: ',_execStart_response) 
+		
+		_execStart_response = invoke_cli.exec_start(exec_id = _execCreate_response)					    
+		print ('_execStart_response: ',_execStart_response) 
 
+		
 if __name__ == "__main__":
 	
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	print dir_path
 	
 	obj = addFile()
-	name = {'container_name' : 'evil_brattain'}
+	name = {'container_name' : 'nostalgic_bhabha'}
 	obj.startContainer_toAddFile(**name)
 	
-	data = {'container_name':'evil_brattain',\
+	data = {'container_name':'nostalgic_bhabha',\
 	'containerpath':'/home/',\
 	'local_path':'/home/parallels/Downloads/for_testing.tar'}
 	obj.copyFileTo_container(**data)
